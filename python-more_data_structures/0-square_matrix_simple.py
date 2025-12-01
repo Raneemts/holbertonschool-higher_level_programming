f3fc6138be5fc8de61da055ccef65d1a8039399f
raneemts <raneem_alsaqat@hotmail.com>
2025-12-01 15:28:24 +0000

    simple square matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix=[]
+    new_matrix= []
 
     for row in matrix:
-        new_row=[]
-        for num in raw:
+        new_row = []
+        for num in row:
             new_row.append(num * num)
         new_matrix.append(new_row)
 
